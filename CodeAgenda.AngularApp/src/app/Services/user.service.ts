7681816d0308a304bc068678aa5020c74f2fabee
Brian Michel <bmoralescutting@gmail.com>
2025-02-20 15:38:19 -0500

    Servicio de user

--- CodeAgenda.AngularApp/src/app/Services/user.service.ts
+++ CodeAgenda.AngularApp/src/app/Services/user.service.ts
@@ -1,9 +1,33 @@
 import { Injectable } from '@angular/core';
+import { HttpClient } from '@angular/common/http';
+import { Observable} from 'rxjs';
+import { environment } from '../../environments/environment';
+import { ResponseApi } from '../Interfaces/response-api';
+import { User } from '../Interfaces/user';
 
 @Injectable({
   providedIn: 'root'
 })
 export class UserService {
 
-  constructor() { }
+private urlAPI:string = environment.endpoint + "User"
+
+  constructor(private http:HttpClient) { }
+
+  GetAll(): Observable<ResponseApi> {
+    return this.http.get<ResponseApi>(`${this.urlAPI}GetAll`);
+}
+
+Create(request: User): Observable<ResponseApi>{
+  return this.http.post<ResponseApi>(`${this.urlAPI}Create`, request);
+}
+
+Edit(request: User): Observable<ResponseApi>{
+  return this.http.put<ResponseApi>(`${this.urlAPI}Edit`, request);
+}
+
+Delete(id: string): Observable<ResponseApi>{
+  return this.http.delete<ResponseApi>(`${this.urlAPI}Delete/${id}`);
+}
+
 }
--- CodeAgenda.AngularApp/src/environments/environment.ts
+++ CodeAgenda.AngularApp/src/environments/environment.ts
@@ -1,4 +1,4 @@
-export const enviroment = {
+export const environment = {
     production: false,
     endpoint: "https://localhost:5062/api/"
 };
